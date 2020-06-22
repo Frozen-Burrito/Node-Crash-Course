6e296d05b456bad291a3128ec00d9a829ef874ab
FrozenBurrito <f3rm3ndouza@outlook.com>
2020-06-22 12:27:29 -0500

    Part 5
    Install and use packages using npm

---
+++ Part_5/npm_packages.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer(( request, response ) => {
+    
+    // lodash
+    const randNum = _.random(0, 50);
+    console.log(randNum);
+
+    const greet = _.once(() => {
+        console.log('Hello');
+    });
+
+    greet();
+    greet();
+
+    // Set header content type
+    response.setHeader('Content-Type', 'text/html');
+
+    // Return a response
+    // response.write('<h1>This is the response!</h1>');
+    // response.write('<p>A cool response</p>');
+    // response.end();
+
+    // Get the route 
+    let viewsPath = '../views/';
+
+    switch(request.url) {
+        case '/':
+            viewsPath += 'index.html';
+            response.statusCode = 200;
+            break;
+        case '/about':
+            viewsPath += 'about.html';
+            response.statusCode = 200;
+            break;
+        case '/about-us':
+            response.statusCode = 301;
+            response.setHeader('Location', '/about');
+            response.end();
+            break;  
+        default: 
+            viewsPath += '404.html';
+            response.statusCode = 404;
+            break;
+    }
+
+    // Respong with an html file
+    fs.readFile(viewsPath, ( error, data ) => {
+
+        if (error) {
+            console.log(error);
+            response.end();
+        }
+
+        // response.write(data);
+        response.end(data);
+    })
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening on port 3000');
+})
---
+++ README.md
@@ -0,0 +1,15 @@
+# Node.js Crash Course
+
+A repo with all the code from a ~5h Node course, covering all the basics.
+
+> In development
+
+## What I've Learned
+
+I watched and coded along the course, as well as coding and researching further about node. 
+
+I've learned: 
+* Node.js basics, such as the file system and modules
+* How to create a simple server
+* Handle requests and responses
+* Basic routing and redirects
